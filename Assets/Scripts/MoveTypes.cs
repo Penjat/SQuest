168ec6dea274b49b38c0bc8989bc0227ad9bb35b
Penjat <theSpencerSymington@Gmail.com>
2019-11-08 12:38:10 -0800

    Added move types, move type helper.

---
+++ Assets/Scripts/MoveTypes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveType {
+    Hand, Mouth, Ass, Breasts
+};
+
+public class MoveTypeHelper{
+    public static MoveType FindMoveType(int i){
+        switch(i){
+            case 0:
+            return MoveType.Hand;
+            case 1:
+            return MoveType.Mouth;
+            case 2:
+            return MoveType.Ass;
+            case 3:
+            return MoveType.Breasts;
+            default:
+            return MoveType.Hand;
+        }
+    }
+}
