567ba10275200f5caac9bcc2be9d38a4818a7604
Penjat <theSpencerSymington@Gmail.com>
2019-11-23 10:00:56 -0800

    changed how moves are added.

---
+++ Assets/Scripts/Moves/Blowjob.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blowjob : Move {
+    public Blowjob(){
+        _name = "Blowjob";
+        _dmg = 8;
+        _areaAffect = false;
+        _primaryType = MoveType.Mouth;
+        _partsUsed = new HashSet<MoveType>{MoveType.Mouth};
+    }
+}
--- Assets/Scripts/Moves/Move.cs
+++ Assets/Scripts/Moves/Move.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move {
+public abstract class Move {
 
     public string _name;
+    public MoveType _primaryType;
     public HashSet<MoveType> _partsUsed;
     public bool _areaAffect;
     public int _dmg;
 
-    public Move(string name, HashSet<MoveType> partsUsed,int dmg, bool areaAffect=false){
-        _name = name;
-        _partsUsed = partsUsed;
-        _dmg = dmg;
-        _areaAffect = areaAffect;
+    public Move(){
+
     }
     public bool CheckLocked(IDictionary<MoveType,Move> partsBeingUsed){
         //returns true if this move requires a part that is used already
