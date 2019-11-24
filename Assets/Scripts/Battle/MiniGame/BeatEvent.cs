7c68b9e63bf31210f3de9f7f63c580caed63f0af
Penjat <theSpencerSymington@Gmail.com>
2019-11-24 12:31:40 -0800

    created beat and note classes.

---
+++ Assets/Scripts/Battle/MiniGame/BeatEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatEvent {
+    int _duration;
+    int _numNotes;
+    public BeatEvent(int duration, int numNotes){
+        _duration = duration;
+        _numNotes = numNotes;
+    }
+    public int GetDuration(){
+        return _duration;
+    }
+    public int GetNumNotes(){
+        return _numNotes;
+    }
+}
---
+++ Assets/Scripts/Battle/MiniGame/Note.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note {
+    int _duration;
+    bool _isRest;
+    public Note(int duration, bool isRest){
+        _duration = duration;
+        _isRest = isRest;
+    }
+}
