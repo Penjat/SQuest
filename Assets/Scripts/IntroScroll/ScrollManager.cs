b1579f0b54645e632fa68abda786ad033a889e3f
Penjat <theSpencerSymington@Gmail.com>
2019-12-07 12:22:50 -0800

    added scroll scene.

---
+++ Assets/Scripts/IntroScroll/ScrollManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollManager : MonoBehaviour {
+
+    void Start() {
+        Debug.Log("showing title scroll");
+
+    }
+
+}
