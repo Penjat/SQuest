4f29299c801700526fa49b03f6c0b2279cbf4daa
Penjat <theSpencerSymington@Gmail.com>
2019-12-09 16:19:36 -0800

    set up for content pages.

---
+++ Assets/Scripts/DEMO/ContentPage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContentPage : MonoBehaviour {
+    
+    public Text _displayText;
+
+    public void Appear(){
+        //TODO do with animation
+        gameObject.SetActive(true);
+    }
+    public void Close(){
+        //TODO do with animation
+        gameObject.SetActive(false);
+    }
+    public void SetText(string text){
+        //sets the rich text to be displayed
+    }
+}
