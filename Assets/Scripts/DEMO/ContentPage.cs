e221e61f7e2a23fd09448de3630face0a48e4255
Penjat <theSpencerSymington@Gmail.com>
2019-12-11 08:00:08 -0800

    animate in content page.

--- Assets/Scripts/DEMO/ContentPage.cs
+++ Assets/Scripts/DEMO/ContentPage.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class ContentPage : MonoBehaviour {
 
     public Text _displayText;
+    public GameObject _scrollView;
+    public GameObject _survey;
+    public GameObject _mailing;
 
     public void Appear(){
         //TODO do with animation
