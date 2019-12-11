1c2c5b7a7485ee87b3c71088df7ceb7d14cfc4b4
Penjat <theSpencerSymington@Gmail.com>
2019-12-11 12:54:07 -0800

    override moves for new enemy.

--- Assets/Scripts/Battle/Enemies/DickSnake.cs
+++ Assets/Scripts/Battle/Enemies/DickSnake.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StringMethods;
+using System;
 
 public class DickSnake : Enemy {
     public override void SetUp(EnemyDelegate enemyDelegate){
@@ -10,5 +12,17 @@ public class DickSnake : Enemy {
     }
     public override string GetName(){
         return "Dick-Snake";
+    }
+    public override void TakeTurn(){
+        Debug.Log("taking my turn");
+        //Dont attack if was targeted last turn
+        if(_usedMoves.Count > 0){
+            DoneTurn();
+            return;
+        }
+        _delegate.EnemyMsg("The " + GetName().ColorFor(Entity.ENEMY) + " begins jerking itself off...");
+        _dmgToDo = 2;
+         Action done = DoneTurn;
+         StartCoroutine(WaitFor(1.0f, done) );
     }
 }
