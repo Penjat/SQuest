0cbf2f9e9edfba1e63e9df1680ace3b0278ecc56
Penjat <theSpencerSymington@Gmail.com>
2019-12-12 11:44:28 -0800

    added in body targets for imp.

--- Assets/Scripts/Battle/Enemies/Imp.cs
+++ Assets/Scripts/Battle/Enemies/Imp.cs
@@ -6,8 +6,9 @@ public class Imp : Enemy {
     public override void SetUp(EnemyDelegate enemyDelegate){
         _maxArousal = 4.0f;
         _curArousal = 4.0f;
-        
         _maxClimax = 8.0f;
+        _bodyTargets = new HashSet<BodyTarget>{new BodyTarget(TargetType.Penis)};
+
         //set stats before base setup
         base.SetUp(enemyDelegate);
     }
