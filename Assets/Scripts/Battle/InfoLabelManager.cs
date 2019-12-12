9ef06993dd448a62c8f0b3d5fc3513c67f9ec5e7
Penjat <theSpencerSymington@Gmail.com>
2019-12-12 07:58:15 -0800

    refactored InfoLabelManager to use new SetTextMethods.

--- Assets/Scripts/Battle/InfoLabelManager.cs
+++ Assets/Scripts/Battle/InfoLabelManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using StringMethods;
 
 public class InfoLabelManager : MonoBehaviour{
-    public Text _infoLabel;
+    public TextTyper _infoLabel;
 
     private PlayerActionManager _playerActionManager;
 
@@ -49,32 +49,32 @@ public class InfoLabelManager : MonoBehaviour{
 
     //---------------------Show Methods-------------------------------
     public void ShowBlank(){
-        _infoLabel.text = "";
+        _infoLabel.SetText("");
     }
     public void ShowTargetsForMove(Move move){
         IEnemy[] targeted = _playerActionManager.GetTargetsFor(move);
         string moveName = move.GetName().ColorFor(Entity.MOVE);
         string enemyName = targeted[0].GetName().ColorFor(Entity.ENEMY);
-        _infoLabel.text = "using " + moveName + " on " + enemyName;
+        _infoLabel.SetText("using " + moveName + " on " + enemyName);
     }
     public void ShowTargetEnemy(IEnemy enemy){
         string moveName = _playerActionManager.GetCurMove().GetName().ColorFor(Entity.MOVE);
         string enemyName = enemy.GetName().ColorFor(Entity.ENEMY);
-        _infoLabel.text = "use " + moveName + " on " + enemyName;
+        _infoLabel.SetText("use " + moveName + " on " + enemyName);
     }
     public void ShowBlockedEnemy(IEnemy enemy){
         //string moveName = _playerActionManager.GetCurMove().GetName().ColorFor(Entity.MOVE);
         //TODO get targeted move
         string enemyName = enemy.GetName().ColorFor(Entity.ENEMY);
-        _infoLabel.text = enemyName + " is already targeted.";
+        _infoLabel.SetText(enemyName + " is already targeted.");
     }
     public void ShowMoveNoTarget(){
         //a move has been selected, but is not over an enemy
         string moveName = _playerActionManager.GetCurMove().GetName().ColorFor(Entity.MOVE);
         string instructions = "\ndouble click to cancel".ColorFor(Entity.INSTRUCTIONS);
-        _infoLabel.text = "use " + moveName + " on..." + instructions;
+        _infoLabel.SetText("use " + moveName + " on..." + instructions);
     }
     public void ShowMsg(string msg){
-        _infoLabel.text = msg;
+        _infoLabel.SetText(msg);
     }
 }
