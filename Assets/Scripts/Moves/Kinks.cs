025bd3f4d87896e886dfd47de65775b1e84b5b8d
Penjat <theSpencerSymington@Gmail.com>
2019-12-17 10:03:08 -0800

    added a kinks enum.

---
+++ Assets/Scripts/Moves/Kinks.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Kink{
+    Sadist, Masocist, HumiliateTarget, HumilateOther, CumTarget, CumOther, Foot, Bonage
+};
